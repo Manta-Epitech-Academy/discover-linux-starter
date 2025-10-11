53c0ed0597a97bf01560a2a832f90692202cd724
Kevin C <kevin.cazal@epitech.eu>
2025-10-11 06:41:34 +0000

    Fix: fix debug_run script

--- v86lab/debug_run.sh
+++ v86lab/debug_run.sh
@@ -2,4 +2,4 @@
 
 IMAGE_NAME=i386/alpine-v86-debug
 docker build . --file Dockerfile.alpine_image --platform linux/386 --rm --tag "$IMAGE_NAME"
-docker run -it --rm --platform linux/386 $IMAGE_NAME login -f user42
+docker run -it --rm --platform linux/386 $IMAGE_NAME su user42 -c 'cd /home/user42/shell_rpg_engine; ./install.sh engine.py; export PATH=/tmp/bin:$PATH ;/bin/ash'
